5794f499953c98a15a32b73c52b26414f9079f1c
magnusforbord <magnus_ff@hotmail.com>
2024-02-10 10:43:53 +0000

    Working script without mongodb and telegram

--- eurohandballAPI.js
+++ eurohandballAPI.js
@@ -2,58 +2,76 @@ const axios = require('axios');
 const dayjs = require('dayjs');
 
 async function fetchMatchDetails(matchId) {
-    console.log(`Fetching match details for matchId: ${matchId}`);
     const url = `https://ehfeuro.eurohandball.com/umbraco/Api/MatchDetailApi/GetMatchDetailsAsync?matchId=${matchId}&culture=en-US&contentId=51748`;
     try {
         const response = await axios.get(url);
-
-        // Transform the fetched match details here
         const matchDetails = response.data.matchDetails;
+        const players = matchDetails.details.homeTeam.players.concat(matchDetails.details.guestTeam.players).map(player => ({
+            id: player.id,
+            name: `${player.person.firstName} ${player.person.lastName}`,
+            position: player.playingPosition,
+            goals: player.score.goals,
+        }));
+
+
         const transformedData = {
             id: matchDetails.details.matchID,
             away: {
                 id: matchDetails.details.guestTeam.team.id,
-                name: matchDetails.details.guestTeam.team.fullName
+                name: matchDetails.details.guestTeam.team.fullName,
+                players: matchDetails.details.guestTeam.players.map(player => ({
+                    id: player.id,
+                    name: `${player.person.firstName} ${player.person.lastName}`,
+                    position: player.playingPosition,
+                    goals: player.score.goals,
+                })),
             },
             date: new Date(matchDetails.details.venue.date.utc).getTime(),
             home: {
                 id: matchDetails.details.homeTeam.team.id,
-                name: matchDetails.details.homeTeam.team.fullName
+                name: matchDetails.details.homeTeam.team.fullName,
+                players: matchDetails.details.homeTeam.players.map(player => ({
+                    id: player.id,
+                    name: `${player.person.firstName} ${player.person.lastName}`,
+                    position: player.playingPosition,
+                    goals: player.score.goals,
+                })),
             },
             league: matchDetails.details.comp.name,
-            // Assuming you have a way to determine missing and present players
-            // missingPlayers: {...},
-            // presentPlayers: {...},
+            allPlayers: players,
         };
 
-        console.log(JSON.stringify(transformedData, null, 2))
-        return transformedData
+        return transformedData;
     } catch (error) {
         console.error(`Request failed for matchId ${matchId}: ${error}`);
         return null;
     }
 }
 
+
 async function fetchTeamRoster(clubId, competitionIds, teamName) {
     for (const competitionId of competitionIds) {
         const url = `https://www.eurohandball.com/umbraco/Api/ClubDetailsApi/GetPlayers?competitionId=${competitionId}&clubId=${clubId}&roundId=kWvDaphagqoomWsHrgovow&culture=en-US&contentId=1528`;
-        console.log(url);
         try {
             const response = await axios.get(url);
-            if (response.data && response.data.players && response.data.players.length > 0) {
-                console.log(`Fetched roster successfully for ${teamName} with competitionId: ${competitionId}`);
+            if (response.data && (response.data.players.length > 0 || response.data.goalKeepers.length > 0)) {
+                // Process both players and goalkeepers
+                const allPlayers = [...response.data.players, ...response.data.goalKeepers];
 
-                // Extract player names and goals
-                const playerNamesAndGoals = response.data.players.map(player => ({
-                    name: `${player.person.firstName} ${player.person.lastName}`,
-                    goals: player.score.goals
-                }));
+                const playerNamesAndStats = allPlayers.map(player => {
+                    const isGoalkeeper = player.isGoalkeeper; // Adjust based on the actual field indicating a goalkeeper
+                    const stats = isGoalkeeper ? { saves: player.score.goalkeeperSaves } : { goals: player.score.goals };
 
-                console.log(`${teamName} roster:`, playerNamesAndGoals);
+                    return {
+                        name: `${player.person.firstName} ${player.person.lastName}`,
+                        position: player.playingPosition,
+                        stats: stats
+                    };
+                });
 
                 return {
                     teamId: clubId,
-                    roster: playerNamesAndGoals
+                    roster: playerNamesAndStats
                 };
             }
         } catch (error) {
@@ -65,20 +83,19 @@ async function fetchTeamRoster(clubId, competitionIds, teamName) {
 }
 
 
+
+
 async function fetchMatchIds() {
-    console.log(`Fetching match IDs for date: 2024-02-10`);
     const url = "https://www.eurohandball.com/umbraco/Api/LiveScoreApi/GetLiveScoreMatchesAsync?culture=en-US&contentId=1069&pastDays=0&upcomingMatches=15";
-    const testingDate = '2024-02-10'
+    const today = dayjs().format('YYYY-MM-DD')
     try {
         const response = await axios.get(url);
-        console.log(`Fetched match IDs successfully for date: ${testingDate}`);
 
         const json_data = response.data;
-        console.log(json_data)
         const match_ids = [];
 
         json_data.days.forEach(day => {
-            if (day.dayDatumFormatted === testingDate) {
+            if (day.dayDatumFormatted === today) {
                 day.liveScoreMatches.forEach(match => {
                     match_ids.push(match.match.matchID);
                 });
@@ -86,9 +103,9 @@ async function fetchMatchIds() {
         });
 
         if (match_ids.length === 0) {
-            console.log(`No matches found for date: ${testingDate}`);
+            console.log(`No matches found for date: ${today}`);
         } else {
-            console.log(`Total found match IDs for date: ${testingDate}: ${match_ids.join(', ')}`);
+            console.log(`Total found match IDs for date: ${today}: ${match_ids.join(', ')}`);
         }
 
         return match_ids;
@@ -98,34 +115,59 @@ async function fetchMatchIds() {
     }
 }
 
+function comparePlayers(teamRoster, matchPlayers) {
+    // Creating a map of match players for efficient lookup
+    const matchPlayerMap = new Map(matchPlayers.map(player => [player.name, player]));
+
+    // Filtering the team roster to find players not in matchPlayerMap
+    return teamRoster.roster.filter(player => {
+        return !matchPlayerMap.has(player.name);
+    }).map(player => {
+        // Adjusted to include either goals or saves based on player stats
+        const stats = player.stats.saves !== undefined ? {saves: player.stats.saves} : {goals: player.stats.goals};
+        return {
+            name: player.name,
+            ...stats
+        };
+    });
+}
+
+
+
+
+
 async function main() {
-    console.log(`Starting main function to process matches for date: 2024-02-10`);
     const matchIds = await fetchMatchIds();
     if (matchIds.length) {
-        console.log(`Processing all match IDs for date: 2024-02-10`);
 
-        // Iterate over each matchId and process it
         for (const matchId of matchIds) {
-            console.log(`Processing match details for matchId: ${matchId}`);
             const matchData = await fetchMatchDetails(matchId);
             if (matchData) {
-                console.log(`Successfully processed match details for matchId: ${matchId}`);
+                if (matchData.home.players.length === 0 || matchData.away.players.length === 0) {
+                    console.log("No players on match page yet")
+                    continue
+                }
                 const competitionIds = ['s9H5PdMUfyxj4Ap4QMTvsQ', 'VOnXVrhoU4vff13IlFgt_w', 'EwoH_yk0xYpV1I73lyx4FQ'];
 
-                // Fetch and log roster for the home team
-                console.log(`Fetching roster for home team: ${matchData.home.name}`);
-                await fetchTeamRoster(matchData.home.id, competitionIds, matchData.home.name);
+                const homeRoster = await fetchTeamRoster(matchData.home.id, competitionIds, matchData.home.name);
+                const awayRoster = await fetchTeamRoster(matchData.away.id, competitionIds, matchData.away.name);
 
-                // Fetch and log roster for the away team
-                console.log(`Fetching roster for away team: ${matchData.away.name}`);
-                await fetchTeamRoster(matchData.away.id, competitionIds, matchData.away.name);
-            } else {
-                console.log(`Failed to fetch details or process match ID: ${matchId}`);
+
+                // Inside your main function, after calling comparePlayers
+                if (homeRoster && awayRoster) {
+                    const missingPlayersHome = comparePlayers(homeRoster, matchData.home.players);
+                    const missingPlayersAway = comparePlayers(awayRoster, matchData.away.players);
+
+                    console.log(`Missing players for home team (${matchData.home.name}):`, missingPlayersHome);
+                    console.log(`Missing players for away team (${matchData.away.name}):`, missingPlayersAway);
+                } else {
+                    console.log(`Failed to fetch rosters for one or both teams.`);
+                }
+
             }
         }
-    } else {
-        console.log("No match IDs were found for date: 2024-02-10");
     }
 }
 
-main();
+
+main().then(r => console.log("Bot finished running"));
